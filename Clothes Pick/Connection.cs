46e4e1571e142962f6c712eb528aa82380fabfef
cristicretan <cristi.cris9975@yahoo.com>
2017-01-23 21:22:20 +0200

    Last commit.

--- Clothes Pick/Connection.cs
+++ Clothes Pick/Connection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,8 @@ namespace OODB
 {
     public class Connection
     {
-        public static string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        public static string path2 = path + @"\Clothes Pick\Clothes Pick\DB.mdf";
-        public static string connection_string = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="+ path2 + ";Integrated Security=True;Connect Timeout=30";
+        public static string path = Path.Combine(Environment.CurrentDirectory, @"") + @"\DB.mdf";
+        public static string connection_string = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="+ path + ";Integrated Security=True;Connect Timeout=30";
     }
 }
 
